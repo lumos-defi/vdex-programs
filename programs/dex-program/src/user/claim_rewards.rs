4b519a2f08065dd951bee21ece9b01ade71a8541
ezhui <dennis.hui.zhou@gmail.com>
2023-04-24 21:04:19 +0800

    fix: add compound period

--- programs/dex-program/src/user/claim_rewards.rs
+++ programs/dex-program/src/user/claim_rewards.rs
@@ -108,8 +108,11 @@ pub fn handler(ctx: Context<ClaimRewards>, amount: u64) -> DexResult {
 
     let price_feed = &ctx.accounts.price_feed.load()?;
 
-    let reward_asset_debt =
-        dex.update_staking_pool(&ctx.remaining_accounts[0..assets_oracles_len], price_feed)?;
+    let reward_asset_debt = dex.update_staking_pool(
+        &ctx.remaining_accounts[0..assets_oracles_len],
+        price_feed,
+        true,
+    )?;
     require!(reward_asset_debt == 0, DexError::InsufficientSolLiquidity);
 
     let (vdx_vested, claimable) = us.borrow_mut().claim_rewards(&mut dex, amount)?;
